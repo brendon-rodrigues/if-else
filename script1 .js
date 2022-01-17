745382442d3d0e3ea84b218db5aceaec1f9475cd
Brendon <rodriguesbrendon861@gmail.com>
2022-01-17 19:56:18 -0300

    exercicio finalizado

--- script1 .js
+++ script1 .js
@@ -6,12 +6,18 @@ const imHuman = true
 if (age > 18) {
     console.log('Sua idade é maior que 18 anos')
 
+} else {
+    console.log('Sua idade é menor que 18 anos')
 }
-
 if (age > 18 && imHuman) {
     console.log('Você atingiu a maior idade e é humano.')
+} else {
+    console.log('Você não possui a maior idade e não é humano.')
 }
+
 
 if (birthdayMonth == 'janeiro' || birthdayMonth == 'dezembro') {
     console.log('seu aniversário é em: ' + birthdayMonth)
+} else {
+    console.log('Seu aniversário é em:')
 }
