1883bce2f157ff15ba075f9c84e8d6ca369ea039
Brendon <rodriguesbrendon861@gmail.com>
2022-01-18 11:41:04 -0300

    atualizado

--- script1 .js
+++ script1 .js
@@ -3,19 +3,16 @@ let age = "23"
 const birthdayMonth = "janeiro"
 const imHuman = true
 
-if (age > 18) {
+if (age >= 18) {
     console.log('Sua idade é maior que 18 anos')
-
 } else {
     console.log('Sua idade é menor que 18 anos')
 }
-if (age > 18 && imHuman) {
+if (age >= 18 && imHuman) {
     console.log('Você atingiu a maior idade e é humano.')
 } else {
     console.log('Você não possui a maior idade e não é humano.')
 }
-
-
 if (birthdayMonth == 'janeiro' || birthdayMonth == 'dezembro') {
     console.log('seu aniversário é em: ' + birthdayMonth)
 } else {
